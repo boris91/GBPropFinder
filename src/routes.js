364057ab8eac3cbfe2763b8191d7fea94c0ea683
Boris Karbanovich <bacsus_beacks@mail.ru>
2016-12-20 10:07:10 +0300

    [modules]: removed Home as redundant.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
-//import HomeView from './modules/home/view';
 import SearchView from './modules/search/view';
 
 export const initialRoute = {
 	index: 0,
 	title: 'Property seeker',
-	component: SearchView//HomeView
+	component: SearchView
 };

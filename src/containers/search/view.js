37fc9743a9dba70aa2e96a1584eda1bef520a2e9
Boris Karbanovich <bacsus_beacks@mail.ru>
2017-01-20 19:35:24 +0300

    [search]: prevented searching by GPS location (due to Android bug).

--- src/containers/search/view.js
+++ src/containers/search/view.js
@@ -8,9 +8,7 @@ export default class Search extends React.Component {
 		super(props);
 
 		this.onQueryChange = this.onQueryChange.bind(this);
-		this.onSearchByGps = this.onSearchByGps.bind(this);
 		this.onGoPress = this.onGoPress.bind(this);
-		this.onLocationPress = this.onLocationPress.bind(this);
 	}
 
 	render() {
@@ -28,19 +26,10 @@ export default class Search extends React.Component {
 					<Fld style={_.queryField} placeholder={queryHolder} value={query} onChange={this.onQueryChange}/>
 					<Btn style={_.button} text="Go" onPress={this.onGoPress}/>
 				</Div>
-				<Div style={_.flowRight}>
-					<Btn style={_.button} text="Location" onPress={this.onLocationPress}/>
-				</Div>
 				<Img style={_.houseImage} source={imageSrc}/>
 				{error ? <Txt>{error}</Txt> : null}
 			</Div>
 		);
-	}
-
-	async onSearchByGps(location) {
-		const { coords: { latitude, longitude } } = location;
-		await this.setSearchQuery(`${latitude},${longitude}`);
-		this.navTo('search-results');
 	}
 
 	onQueryChange(event) {
@@ -49,9 +38,5 @@ export default class Search extends React.Component {
 
 	onGoPress() {
 		this.navTo('search-results');
-	}
-
-	onLocationPress() {
-		navigator.geolocation.getCurrentPosition(this.onSearchByGps);
 	}
 };
